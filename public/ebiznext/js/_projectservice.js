881e5ec3005a17517f3715a31ddf0cb6d7e7a3ac
bedouimoez <moez.bedoui@ebiznext.com>
2013-11-07 17:46:45 +0100

    Delete _projectservice.js

--- public/ebiznext/js/_projectservice.js
+++
@@ -1,5 +0,0 @@
-var projectService = angular.module('projectService', ['ngResource']);
-projectService.factory('Project', ['$resource', function($resource) {
-        var url = 'http://localhost:8080/api/posts/:id';
-        return $resource(url,{method:'PUT'} ,{ id: '@id' });
-    }]);
