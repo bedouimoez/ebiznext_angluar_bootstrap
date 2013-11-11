1fe44d56dbac2c9143a55990d7515a91016a8baf
moezbedouifst <moez.bedoui@ebiznext.com>
2013-11-11 21:56:07 +0100

    commit

--- public/test/test.js
+++
@@ -1,8 +0,0 @@
-describe('rechercher emloyes', function() {
- beforeEach(function() {
- browser().navigateTo('http://localhost:3000/employes');
- });
- it('should filter results', function() {
- input('filterId').enter('chris');
-  });
-});
